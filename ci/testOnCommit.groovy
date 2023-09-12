243ad1f27929a59206f8273c7421aa8694df1e28
Karolina Sankiewicz <ksankiew@JordanRY-mobl.ger.corp.intel.com>
2023-09-12 13:06:26 +0200

    CVS-114339 on commit tests

---
+++ ci/testOnCommit.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent {
+      label 'ovmscheck'
+    }
+    stages {
+        stage('Configure') {
+          steps {
+            script {
+              checkout scm
+              shortCommit = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
+              echo shortCommit
+            }
+          }     
+        }   
+        stage("Run tests on commit") {
+          steps {
+              sh """
+              env
+              """
+              echo shortCommit
+              build job: "ovmsci/users/rasapala/OpenVinoMediapipe", parameters: [[$class: 'StringParameterValue', name: 'MEDIACOMMIT', value: shortCommit]]
+          }    
+        }
+    }
+}
+
