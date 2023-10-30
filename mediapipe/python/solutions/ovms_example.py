cd44e46f5eb5958b20fb1e681830906c496e98a2
Rafal Sapala <rafal.a.sapala@intel.com>
2023-10-30 17:32:27 +0100

    Working minimal

--- mediapipe/python/solutions/ovms_example.py
+++ mediapipe/python/solutions/ovms_example.py
@@ -11,14 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""MediaPipe Face Detection."""
+"""Ovms Object Detection."""
 
-import enum
-from typing import NamedTuple, Union
-
-import numpy as np
-from mediapipe.framework.formats import detection_pb2
-from mediapipe.framework.formats import location_data_pb2
 from mediapipe.calculators.ovms import openvinoinferencecalculator_pb2
 from mediapipe.calculators.ovms import openvinomodelserversessioncalculator_pb2
 from mediapipe.python.solution_base import SolutionBase
@@ -26,14 +20,10 @@ from mediapipe.python.solution_base import SolutionBase
 _FULL_GRAPH_FILE_PATH = 'mediapipe/modules/object_detection_ovms/object_detection_ovms.binarypb'
 
 class OvmsObjectDetection(SolutionBase):
-  """MediaPipe Face Detection.
+  """Ovms Object Detection.
 
-  MediaPipe Face Detection processes an RGB image and returns a list of the
-  detected face location data.
-
-  Please refer to
-  https://solutions.mediapipe.dev/face_detection#python-solution-api
-  for usage examples.
+  Ovms Object Detection processes an input video returns output video
+  with detectec objects.
   """
   """
   Oryginal params in desktop example
@@ -41,16 +31,7 @@ class OvmsObjectDetection(SolutionBase):
   --input_side_packets "input_video_path=/mediapipe/mediapipe/examples/desktop/object_detection/test_video.mp4,output_video_path=/mediapipe/tested_video.mp4
   """
   def __init__(self):
-    """Initializes a MediaPipe Object Detection object.
-
-    Args:
-      min_detection_confidence: Minimum confidence value ([0.0, 1.0]) for face
-        detection to be considered successful. See details in
-        https://solutions.mediapipe.dev/face_detection#min_detection_confidence.
-      model_selection: 0 or 1. 0 to select a short-range model that works
-        best for faces within 2 meters from the camera, and 1 for a full-range
-        model best for faces within 5 meters. See details in
-        https://solutions.mediapipe.dev/face_detection#model_selection.
+    """Initializes a Ovms Object Detection object.
     """
 
     binary_graph_path = _FULL_GRAPH_FILE_PATH
@@ -61,19 +42,6 @@ class OvmsObjectDetection(SolutionBase):
         {'input_video_path':'/mediapipe/mediapipe/examples/desktop/object_detection/test_video.mp4',
          'output_video_path':'/mediapipe/tested_video.mp4'})
 
-  def process(self, image: np.ndarray) -> NamedTuple:
-    """Processes an RGB image and returns a list of the detected face location data.
-
-    Args:
-      image: An RGB image represented as a numpy ndarray.
-
-    Raises:
-      RuntimeError: If the underlying graph throws any error.
-      ValueError: If the input image is not three channel RGB.
-
-    Returns:
-      A NamedTuple object with a "detections" field that contains a list of the
-      detected face location data.
-    """
-
+  def process(self):
+    self._graph.wait_until_done()
     return None
