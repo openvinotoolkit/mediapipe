47fbab65b0aabd689cfe42b6b90cad705de0939d
Rafal Sapala <rafal.a.sapala@intel.com>
2023-12-20 14:34:28 +0100

    Loop fix

--- mediapipe/calculators/geti/serialization/serialization_calculators.h
+++ mediapipe/calculators/geti/serialization/serialization_calculators.h
@@ -25,7 +25,7 @@
 #include "mediapipe/framework/port/opencv_core_inc.h"
 #include "mediapipe/framework/port/opencv_imgcodecs_inc.h"
 #include "mediapipe/framework/port/status.h"
-#include "serialization/result_serialization.h"
+#include "result_serialization.h"
 
 namespace mediapipe {
 
