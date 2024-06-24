d9fa8dad8e73fec13b6c8a22a3525d43b3a74704
Ronald Hecker <ronald.hecker@intel.com>
2024-06-24 10:05:52 +0200

    Update utils calculators

--- mediapipe/calculators/geti/utils/data_structures.h
+++ mediapipe/calculators/geti/utils/data_structures.h
@@ -1,18 +1,3 @@
-/**
- *  INTEL CONFIDENTIAL
- *
- *  Copyright (C) 2023 Intel Corporation
- *
- *  This software and the related documents are Intel copyrighted materials, and
- * your use of them is governed by the express license under which they were
- * provided to you ("License"). Unless the License provides otherwise, you may
- * not use, modify, copy, publish, distribute, disclose or transmit this
- * software or the related documents without Intel's prior written permission.
- *
- *  This software and the related documents are provided as is, with no express
- * or implied warranties, other than those that are expressly stated in the
- * License.
- */
 #ifndef DATA_STRUCTURES_H
 #define DATA_STRUCTURES_H
 
@@ -52,11 +37,23 @@ struct RotatedRectanglePrediction {
   cv::RotatedRect shape;
 };
 
+struct Circle {
+  float x;
+  float y;
+  float radius;
+};
+
+struct CirclePrediction {
+  std::vector<LabelResult> labels;
+  Circle shape;
+};
+
 struct InferenceResult {
   std::vector<RectanglePrediction> rectangles;
   std::vector<RotatedRectanglePrediction> rotated_rectangles;
   std::vector<PolygonPrediction> polygons;
   std::vector<SaliencyMap> saliency_maps;
+  std::vector<CirclePrediction> circles;
   cv::Rect roi;
 };
 }  // namespace geti
