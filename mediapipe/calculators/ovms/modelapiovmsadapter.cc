acbffaa90dff241cd17434e28c090e1b1fc6c3a7
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2024-11-04 14:20:41 +0100

    error handling

--- mediapipe/calculators/ovms/modelapiovmsadapter.cc
+++ mediapipe/calculators/ovms/modelapiovmsadapter.cc
@@ -176,7 +176,16 @@ void OVMSInferenceAdapter::loadModel(const std::shared_ptr<const ov::Model>& mod
     }
     const ov::AnyMap* servableMetadataRtInfo;
     ASSERT_CAPI_STATUS_NULL(OVMS_ServableMetadataInfo(servableMetadata, reinterpret_cast<const void**>(&servableMetadataRtInfo)));
-    this->modelConfig = (*servableMetadataRtInfo).at("model_info").as<ov::AnyMap>();
+    try {
+        if ((*servableMetadataRtInfo).count("model_info") == 0) {
+            this->modelConfig = ov::AnyMap{};
+        } else {
+            this->modelConfig = (*servableMetadataRtInfo).at("model_info").as<ov::AnyMap>();
+        }
+    } catch (const std::exception& e) {
+        LOG(INFO) << "Exception occurred while accessing model_info: " << e.what();
+        this->modelConfig = ov::AnyMap{};
+    }
 }
 
 ov::PartialShape OVMSInferenceAdapter::getInputShape(const std::string& inputName) const {
