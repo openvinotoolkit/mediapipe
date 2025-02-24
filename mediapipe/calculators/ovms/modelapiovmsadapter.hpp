38305cd967ed09e959c6d8e127240ac211a18a28
Mariusz Gumowski <mariusz.gumowski@intel.com>
2025-02-24 14:53:31 +0100

    fix ctor

--- mediapipe/calculators/ovms/modelapiovmsadapter.hpp
+++ mediapipe/calculators/ovms/modelapiovmsadapter.hpp
@@ -26,13 +26,15 @@
 #include <adapters/inference_adapter.h>  // model_api/model_api/cpp/adapters/include/adapters/inference_adapter.h
 #include <openvino/openvino.hpp>
 
+#include "ovms.h"  // NOLINT
+
 // here we need to decide if we have several calculators (1 for OVMS repository, 1-N inside mediapipe)
 // for the one inside OVMS repo it makes sense to reuse code from ovms lib
 
 class OVMS_Server_;
 typedef struct OVMS_Server_ OVMS_Server;
-namespace mediapipe {
-namespace ovms {
+
+namespace mediapipe::ovms {
 
 using InferenceOutput = std::map<std::string, ov::Tensor>;
 using InferenceInput = std::map<std::string, ov::Tensor>;
@@ -47,13 +49,22 @@ class OVMSInferenceAdapter : public ::InferenceAdapter {
     std::vector<std::string> inputNames;
     std::vector<std::string> outputNames;
     shapes_min_max_t inShapesMinMaxes;
-    ov::AnyMap modelConfig;
     shapes_min_max_t outShapesMinMaxes;
     std::unordered_map<std::string, ov::element::Type_t> inputDatatypes;
     std::unordered_map<std::string, ov::element::Type_t> outputDatatypes;
+    ov::AnyMap modelConfig;
 
 public:
-    OVMSInferenceAdapter(const std::string& servableName, uint32_t servableVersion = 0, OVMS_Server* server = nullptr);
+    // TODO Windows: Fix definition in header - does not compile in cpp.
+    OVMSInferenceAdapter(const std::string& servableName, uint32_t servableVersion = 0, OVMS_Server* server = nullptr) :
+        servableName(servableName),
+        servableVersion(servableVersion) {
+        if (nullptr != server) {
+            this->cserver = server;
+        } else {
+            OVMS_ServerNew(&this->cserver);
+        }
+    }
     virtual ~OVMSInferenceAdapter();
     InferenceOutput infer(const InferenceInput& input) override;
     void infer(const InferenceInput& input, InferenceOutput& output) override;
@@ -73,5 +84,4 @@ public:
     std::vector<std::string> getOutputNames() const override;
     const ov::AnyMap& getModelConfig() const override;
 };
-}  // namespace ovms
 }  // namespace mediapipe
